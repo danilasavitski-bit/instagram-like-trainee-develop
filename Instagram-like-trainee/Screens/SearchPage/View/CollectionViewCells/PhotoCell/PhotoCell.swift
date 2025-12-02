af694d43331d7d7d73881251c02008f2e30b21d3
Vallner <danila.savitski@innowise.com>
2025-12-02 12:21:01 +0300

    [FIXED] reuse UserCell issue

--- Instagram-like-trainee/Screens/SearchPage/View/CollectionViewCells/PhotoCell/PhotoCell.swift
+++ Instagram-like-trainee/Screens/SearchPage/View/CollectionViewCells/PhotoCell/PhotoCell.swift
@@ -25,6 +25,11 @@ final class PhotoCell: UICollectionViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        photoCellImageView.image = nil
+    }
 
     func configure(
         photoImageURL: URL,
